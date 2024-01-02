1ec8f318823a8ecb6d7838ce1e347b9dc0ff9f2a
Moniruzzaman <107347380+moniruzzaman76@users.noreply.github.com>
2024-01-03 02:05:15 +0600

    code refactor

--- lib/State_holders/video_playList_controller.dart
+++ lib/State_holders/video_playList_controller.dart
@@ -1,34 +1,3 @@
-// import 'package:get/get.dart';
-// import '../data/Utils/urls.dart';
-// import '../data/model/video_player_model.dart';
-// import '../data/network_service/network_caller.dart';
-// import '../data/network_service/network_response.dart';
-// class VideoPlayListController extends GetxController{
-//
-//   bool _videoPlayListInProgress = false;
-//   VideoPlayListModel _videoPlayListModel = VideoPlayListModel();
-//
-//   bool get videoPlayListInProgress => _videoPlayListInProgress;
-//   VideoPlayListModel get videoPlayListModel => _videoPlayListModel;
-//
-//   Future<bool> getVideoPlayList() async {
-//     _videoPlayListInProgress = true;
-//     update();
-//
-//     final NetworkResponse response = await NetWorkCaller().getRequest(Urls.videoListUrl);
-//     _videoPlayListInProgress = false;
-//     update();
-//
-//     if (response.isSuccess) {
-//       _videoPlayListModel = VideoPlayListModel.fromJson(response.body!);
-//       update();
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-// }
-
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import '../data/Utils/urls.dart';
 import '../data/model/video_player_model.dart';
